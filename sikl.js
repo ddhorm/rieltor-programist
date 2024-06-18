07b6340a728b27119e51758ebb6fba4edbc5050c
Doniyor <143365081+ddhorm@users.noreply.github.com>
2024-06-18 23:28:39 +0500

    Add files via upload

---
+++ sikl.js
@@ -0,0 +1,27 @@
+// const arr = ['a', 'b', 'c', ];
+
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// let x = 0;
+// while (x < 3) {
+//     console.log(x);
+//     x++;
+// }
+// let x = 0;
+// while (x < 3) {
+//     console.log(x);
+//     x++;
+//     break;
+
+// }
+// let word = '';
+// while (word.length < 5) {
+//     const value = prompt('Введите букву')
+//     if (value.length === 0) {
+//         break;
+//     }
+//     word = word + value;
+//     console.log(word);
+// }
